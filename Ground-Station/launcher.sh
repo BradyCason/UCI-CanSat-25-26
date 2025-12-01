01f7840650d7b866b1f42f92ac45eaed8d0c24ec
Brady Cason <bradyrcason@gmail.com>
2025-12-01 15:06:01 -0800

    physical gs controls code

---
+++ Ground-Station/launcher.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# launcher.sh
+# navigate to home directory, then to this directory, then execute python script, then back home
+
+cd /home/brcason/UCI-CanSat-25-26/Ground-Station
+sudo python controls.py
+cd /home/brcason/
