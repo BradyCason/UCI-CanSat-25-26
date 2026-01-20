45a69fc413499bac4c180bd0830c02eaca4bddf9
jonnylin22 <linjonathan22@gmail.com>
2026-01-20 13:19:20 -0800

    updated with one XBEE LED flashing

--- Ground-Station/controls.py
+++ Ground-Station/controls.py
@@ -15,27 +15,28 @@ pin_10 = 10 # LED indicator for simulation enable button
 pin_11 = 11 # set coordinates button
 
 pin_5 = 5 # release egg button
-pin_0 = 0 # LED indicator for release egg button
+# pin_0 = 0 # LED indicator for release egg button
 
 pin_13 = 13 # Set time UTC button
-pin_6 = 6   # LED indicator for set time UTC button
+# pin_6 = 6   # LED indicator for set time UTC button
 
 pin_26 = 26 # set time GPS
-pin_19 = 19 # LED indicator for set time GPS button
+# pin_19 = 19 # LED indicator for set time GPS button
+pin_19 = 19 # global LED indicator for XBEE packet sending indication
 
 pin_27 = 27 # release paraglider
-pin_22 = 22 # LED indicator for paraglider release button
+# pin_22 = 22 # LED indicator for paraglider release button
 
 pin_4 = 4 # Release payload
-pin_17 = 17 # LED indicator for payload release button
+# pin_17 = 17 # LED indicator for payload release button
 
 pin_18 = 18 # show graphs
 
 pin_21 = 21 # press to reset state
-pin_20 = 20 # flash led to indicate reseting state packet sending
+# pin_20 = 20 # flash led to indicate reseting state packet sending
 
 pin_16 = 16 # example: calibrate altitude button
-pin_12 = 12 # example: LED indicator for calibrate altitue button
+# pin_12 = 12 # example: LED indicator for calibrate altitue button
 
 # example toggle switch application:
 pin_7 = 7 # example: telemetry toggle on
@@ -63,34 +64,34 @@ GPIO.output(pin_10, GPIO.HIGH)
 GPIO.setup(pin_11, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set coordinates button
 
 GPIO.setup(pin_5, GPIO.IN, pull_up_down=GPIO.PUD_UP) # release egg button
-GPIO.setup(pin_0, GPIO.OUT)
-GPIO.output(pin_0, GPIO.LOW)
+# GPIO.setup(pin_0, GPIO.OUT)
+# GPIO.output(pin_0, GPIO.LOW)
 
 GPIO.setup(pin_13, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set time UTC button
-GPIO.setup(pin_6, GPIO.OUT)
-GPIO.output(pin_6, GPIO.LOW)
+# GPIO.setup(pin_6, GPIO.OUT)
+# GPIO.output(pin_6, GPIO.LOW)
 
 GPIO.setup(pin_26, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set time GPS button
 GPIO.setup(pin_19, GPIO.OUT)
 GPIO.output(pin_19, GPIO.LOW)
 
 GPIO.setup(pin_27, GPIO.IN, pull_up_down=GPIO.PUD_UP) # paraglider release button
-GPIO.setup(pin_22, GPIO.OUT)
-GPIO.output(pin_22, GPIO.LOW)
+# GPIO.setup(pin_22, GPIO.OUT)
+# GPIO.output(pin_22, GPIO.LOW)
 
 GPIO.setup(pin_4, GPIO.IN, pull_up_down=GPIO.PUD_UP) # payload release button
-GPIO.setup(pin_17, GPIO.OUT)
-GPIO.output(pin_17, GPIO.LOW)
+# GPIO.setup(pin_17, GPIO.OUT)
+# GPIO.output(pin_17, GPIO.LOW)
 
 GPIO.setup(pin_18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 GPIO.setup(pin_21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(pin_20, GPIO.OUT)
-GPIO.output(pin_20, GPIO.LOW)
+# GPIO.setup(pin_20, GPIO.OUT)
+# GPIO.output(pin_20, GPIO.LOW)
 
 GPIO.setup(pin_16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(pin_12, GPIO.OUT)
-GPIO.output(pin_12, GPIO.LOW)
+# GPIO.setup(pin_12, GPIO.OUT)
+# GPIO.output(pin_12, GPIO.LOW)
 
 GPIO.setup(pin_7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(pin_8, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -203,14 +204,14 @@ try:
         if state_5 == GPIO.LOW :
             print("Release egg button pressed")
             print("Pin 5 button pressed, do work...")
-            flash_led(pin_0)
-            print("Pin 0 flashes status LED to show sending xbee packet")
+            flash_led(pin_19)
+            print("Pin 19 flashes status LED to show sending xbee packet")
 
         if state_13 == GPIO.LOW :
             print("Set UTC time button pressed")
             print("Pin 13 button pressed, do work...")
-            flash_led(pin_6)
-            print("Pin 6 flashes status LED to show sending xbee packet")
+            flash_led(pin_19)
+            print("Pin 19 flashes status LED to show sending xbee packet")
 
         if state_26 == GPIO.LOW :
             print("Set GPS time button pressed")
@@ -221,14 +222,14 @@ try:
         if state_27 == GPIO.LOW :
             print("Paraglider release button pressed")
             print("Pin 27 button pressed, do work...")
-            flash_led(pin_22)
-            print("Pin 22 flashes status LED to show sending xbee packet")
+            flash_led(pin_19)
+            print("Pin 19 flashes status LED to show sending xbee packet")
 
         if state_4 == GPIO.LOW :
             print("Payload release button pressed")
             print("Pin 4 button pressed, do work...")
-            flash_led(pin_17)
-            print("Pin 17 flashes status LED to show sending xbee packet")
+            flash_led(pin_19)
+            print("Pin 19 flashes status LED to show sending xbee packet")
 
 
         if state_16 == GPIO.LOW :
@@ -236,8 +237,8 @@ try:
             print("Pin 16 active, do work:") 
             print("For example: calibrate altitude")
             # call some other functions  that do the work
-            flash_led(pin_12)
-            print("Pin 12 flashes status LED to show sending xbee packet")
+            flash_led(pin_19)
+            print("Pin 19 flashes status LED to show sending xbee packet")
 
         if state_7 == GPIO.LOW and telemetry_func_called == False:
             print("Telemetry enabled")
@@ -256,8 +257,8 @@ try:
 
         if state_21 == GPIO.LOW :
             print("Pin 21 button pressed, call reset state function")
-            flash_led(pin_20)
-            print("Pin 20 flashes to show sending xbee packet ")
+            flash_led(pin_19)
+            print("Pin 19 flashes to show sending xbee packet ")
 
 
         if state_18 == GPIO.LOW and prev == GPIO.HIGH:
