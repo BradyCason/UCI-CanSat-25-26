65c0f2e4aba3c5af7bc8e83fc773508f33018481
BradyCason <bradyrcason@gmail.com>
2026-06-04 13:38:40 -0400

    Checking figure_1

--- Ground-Station/rpi-ground-station.py
+++ Ground-Station/rpi-ground-station.py
@@ -480,13 +480,12 @@ class GroundStationWindow(QtWidgets.QMainWindow):
         self.title.setText("CanSat Ground Station - TEAM " + TEAM_ID)
 
         # Get telemetry labels
-        print(self.figure_1_2)
-        print(self.figure_1_2.figure_1)
+        print(self.figure_1)
         self.telemetry_labels = {}
         for field in TELEMETRY_FIELDS + ["LOST_PACKET_COUNT"]:
             if field == "PARAGLIDER_EJECTED":
                 continue
-            label = self.top_right_layout.findChild(QtWidgets.QLabel, field) 
+            label = self.figure_1.findChild(QtWidgets.QLabel, field) 
             if (not label):
                 label = self.top_left_layout.findChild(QtWidgets.QLabel, field)
                 if (not label):
