09c311c3c8890a2c074234a9c8ac1e9864e57a11
jonnylin22 <linjonathan22@gmail.com>
2026-06-05 09:44:54 -0700

    servo init

--- Ground-Station/rpi-ground-station.py
+++ Ground-Station/rpi-ground-station.py
@@ -184,10 +184,10 @@ class ControlsThread(QtCore.QThread):
     def servo_calibration_sweep(self):
         for angle in range(181, 0, -1):
             self.set_state_servo_angle(angle, delay=0.015, pwm_off_hold=False)
-        time.sleep(1.5)
+        time.sleep(1)
         for angle in range(0, 181, 30):
             self.set_state_servo_angle(angle, delay=0.6, pwm_off_hold=False)
-            time.sleep(0.5)
+            time.sleep(0.35)
 
         global pwm
         pwm.ChangeDutyCycle(0)
